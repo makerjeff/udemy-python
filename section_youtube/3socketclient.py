f6bb95b8fcb238a8f5cc186c6b6e1dd65119c80f
Jefferson Wu <makerjeffwu@gmail.com>
2017-03-14 18:37:55 -0700

    continue working on 3socketclient

--- section_youtube/3socketclient.py
+++ section_youtube/3socketclient.py
@@ -1,6 +1,8 @@
 import binascii
 import socket
 import sys
+import pickle
+import cv2
 
 # create tcp/ip connection
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -10,11 +12,21 @@ sock.connect(server_address)
 # data
 data = 'Here lies a string of text that will be sent to the server.'
 
+def show_file(data):
+    datum = data
+    imgarr = pickle.loads(datum)
+    return imgarr
+
+    #TODO: use CV2.imshow loop to display image just downloaded
+
 try:
-    # send data
-    print 'sending data: ' + binascii.hexlify(data)
-    sock.sendall(binascii.hexlify(data))
+    # # send data
+    # print 'sending data: ' + binascii.hexlify(data)
+    # sock.sendall(binascii.hexlify(data))
+    show_file(sock.recv(2048))
+
+
 
 finally:
-    print 'closing socket'
+    # print 'closing socket'
     sock.close()
--- section_youtube/3socketserver.py
+++ section_youtube/3socketserver.py
@@ -1,14 +1,21 @@
 # learn more about binascii here:
 # https://docs.python.org/2/library/binascii.html
 
+# potentially use this too: cStringIO (faster version of StringIO)
+# https://docs.python.org/2/library/stringio.html
+
 # go through this sentdex tutorial:
 # https://www.youtube.com/watch?v=wzrGwor2veQ
+
+# TODO: THIS WORKS, sending and receiving arrays over sockets
+# http://stackoverflow.com/questions/7107075/sending-and-receiving-arrays-via-sockets
 
 import binascii
 import socket
 import sys
+import cv2
+import pickle
 
-file = open('../section_17/images/jeff.jpg', 'rb')
 
 # create server
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,6 +25,12 @@ sock.listen(5)
 
 #
 
+def send_image_file(file):
+    img = cv2.imread(file)
+    data = pickle.dumps(img)
+    return data
+
+
 while True:
     print >>sys.stderr, '\n waiting for a connection...'
 
@@ -25,12 +38,16 @@ while True:
     connection, client_address = sock.accept()
 
     try:
-        data = connection.recv(1024)
+        # data = connection.recv(1024)
+        #
+        # # write data back to client.
+        # print >>sys.stderr, 'received "%s"' % data
+        # print 'converting back to string: '
+        # print binascii.unhexlify(data)
 
-        # write data back to client.
-        print >>sys.stderr, 'received "%s"' % data
-        print 'converting back to string: '
-        print binascii.unhexlify(data)
+        print 'sending data...'
+        connection.send(send_image_file('../section_17/images/jeff.jpg'))
+
 
     finally:
         connection.close()
